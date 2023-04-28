a03fdc521b946a6d1d0b56949dd5e897d6bb03a5
Erik Varga <viglew@gmail.com>
2023-04-28 17:36:01 -0400

    css html js updated

--- game12.js
+++ game12.js
@@ -32,6 +32,21 @@ let roll_num = 0;
 let score = 0;
 let moving_sum_dice = 0;
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 function roll() {
     if (roll_btn.value == 'Restart') {
         reset();
